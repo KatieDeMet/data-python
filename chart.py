a74e034e596b69eca2642687196a804a611ebb2d
Katie DeMet <katie.demet7@gmail.com>
2021-07-26 15:16:31 -0500

    updated

--- chart.py
+++ chart.py
@@ -9,11 +9,11 @@ total_straw = 0
 for line in file:
     line = line.rstrip('\n').split(',')
     if line[2] == "Chocolate":
-        total_choc += 1
+        total_choc += round(float(line[3])*float(line[4]), 2)
     elif line[2] == "Vanilla":
-        total_van += 1
+        total_van += round(float(line[3])*float(line[4]), 2)
     else:
-        total_straw += 1
+        total_straw += round(float(line[3])*float(line[4]), 2)
 
 line_x = ["Chocolate", "Vanilla", "Strawberry"]
 line_y = [total_choc, total_van, total_straw]
